0d87ee264799c8b790524dd3ada0dd12eb6be8f1
joshburnett <joshburnett@users.noreply.github.com>
2016-06-23 15:31:37 -0400

    Initial file upload to create repository

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "scanf",
+    version = "1.3",
+    packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = "Josh Burnett",
+    author_email = "scanf@burnettsonline.org",
+    description = "A small scanf-implementation",
+    license = "MIT",
+    keywords = "scanf",
+)
