10fb779097b2fd34825122aacb362592cafeac9b
Joshua Burnett <josh+github@burnettsonline.org>
2018-10-04 08:23:37 -0400

    Bump PyPI status to Production/Stable

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     py_modules=["scanf"],
 
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
 
